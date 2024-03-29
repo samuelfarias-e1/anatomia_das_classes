d0cd452136dc015d4742e11fcaba6359297538c4
samuel farias <samuelcontatoe1@gmail.com>
2024-03-29 17:22:55 -0300

    aula finalizada

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -2,6 +2,19 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String br = "Brasil";
+        double pi = 3.14;
+        int estados_brasileiros = 27;
+        int ano_2000 = 2000;
+
+        System.out.println("br");
+        System.out.println("pi");
+        System.out.println("estados_brasileiros");
+        System.out.println("ano_2000");
+
+        
+
+        
     }
+    
 }
